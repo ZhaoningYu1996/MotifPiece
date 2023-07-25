6f6ed57770166be235079024caf1be047ff945bf
ZhaoningYu1996 <zha0nin9@gmail.com>
2023-07-25 12:51:54 -0500

    add inference

--- utils/motifpiece.py
+++ utils/motifpiece.py
@@ -1,3 +1,5 @@
+import os
+import json
 from utils.utils import get_fragment_mol, get_smiles, sanitize_smiles, get_mol, sanitize
 from tqdm import tqdm
 from scipy.sparse import csr_matrix
@@ -7,15 +9,31 @@ MST_MAX_WEIGHT = 100
 MAX_NCAND = 2000
 
 class MotifPiece:
-    def __init__(self, dataset, pre_transform = None):
+    def __init__(self, dataset = None, vocab_path = None, pre_transform = None):
         self.pre_transform = None
         self.dataset_size = 0
         self.threshold = 100
-        self.motif_vocab = {}
-        self.process(dataset)
+        if os.path.isfile(vocab_path+"motif_vocab.txt"):
+            with open(vocab_path+"motif_vocab.txt", "r") as file:
+                self.motif_vocab = json.load(file)
+        else:
+            self.motif_vocab = {}
+            self.process(dataset)
+            if not os.path.exists(vocab_path):
+                os.makedirs(vocab_path)
+            with open(vocab_path+"motif_vocab.txt", "w") as file:
+                file.write(json.dumps(self.motif_vocab))
         print(f"motif vocabulary: {self.motif_vocab}")
     
     def process(self, dataset):
+        """
+        Generate a motif vocabulary from a dataset
+
+        Parameters
+        ---------
+        dataset: a list of smiles
+            Input dataset that will be processed to extract motifs
+        """
         s_dict_list = []
         v_dict_list = []
         e_dict_list = []
@@ -57,7 +75,7 @@ class MotifPiece:
             
             # Select the best motif candidate
             selected_motif = max(motif_count, key=motif_count.get)
-            # print(selected_motif)
+            print(selected_motif)
             print(motif_count)
             
             
@@ -83,7 +101,6 @@ class MotifPiece:
                         
                         while True:
                             break_all = -1
-                            deleted_subgraph_set = []
                             for subgraph_id, edge_list in s_dict.items():
                                 for j in range(len(edge_list)-1):
                                     for k in range(j+1, len(edge_list)):
@@ -100,24 +117,50 @@ class MotifPiece:
 
                                             key = max(s_dict)
                                             v_dict[key+1] = m
+                                            # print(f"edge_list: {edge_list}")
+                                            # print(f"m: {m}")
+                                            # print(f"s_dict: {s_dict}")
+                                            # print(f"v_dict: {v_dict}")
+                                            # print(f"e_dict: {e_dict}")
+                                            # print(f"e1: {e1}")
+                                            # print(f"e2: {e2}")
 
                                             new_edge = set(s_dict[e1[0]]+s_dict[e1[1]]+s_dict[e2[0]]+s_dict[e2[1]])
                                             new_edge.remove(edge_list[j])
                                             new_edge.remove(edge_list[k])
                                             new_edge = list(new_edge)
                                             s_dict[key+1] = new_edge
+                                            node_list = list(set(list(e1)+list(e2)))
+
+                                            deleted_edge_set = [edge_list[j], edge_list[k]]
 
                                             for e in new_edge:
                                                 edge = e_dict[e]
-                                                for element in edge:
-                                                    if element not in m:
-                                                        e_dict[e] = (element, key+1)
+                                                element1, element2 = list(edge)
+                                                if element1 not in node_list and element2 in node_list:
+                                                    e_dict[e] = (element1, key+1)
+                                                elif element1 in node_list and element2 not in node_list:
+                                                    e_dict[e] = (element2, key+1)
+                                                else:
+                                                    new_value = set(s_dict[key+1])
+                                                    new_value.remove(e)
+                                                    s_dict[key+1] = list(new_value)
+                                                    deleted_edge_set.append(e)
 
-                                            deleted_subgraph_set += list(set([e1[0], e1[1], e2[0], e2[1]]))
+                                            # for e in new_edge:
+                                            #     edge = e_dict[e]
+                                            #     for element in edge:
+                                            #         if element not in node_list:
+                                            #             e_dict[e] = (element, key+1)
+
+                                            deleted_subgraph_set = list(set(list(e1)+list(e2)))
 
                                             for key in deleted_subgraph_set:
                                                 del s_dict[key]
-                                            del e_dict[edge_list[j]], e_dict[edge_list[k]]
+                                            # del e_dict[edge_list[j]], e_dict[edge_list[k]]
+                                            for key in deleted_edge_set:
+                                                del e_dict[key]
+
                                             break_all = 1
                                             break
                                     if break_all == 1:
@@ -131,6 +174,23 @@ class MotifPiece:
     
 
     def initialize(self, mol):
+        """
+        An initialization function for motif vocabulary generation
+
+        Parameters
+        ---------
+        mol: RDKit object
+            A mol that will be process for the next process
+        
+        Output
+        -----
+        s_dict: A dictionary
+            A dictionary with format subgraph_id: edge_indices_list
+        v_dict: A dictionary
+            A dictionary with format subgraph_id: constituent origianl node indices of the subgraph
+        e_dict: A dictionary
+            A dictionary with format edge_id: (start_node_id, end_node_id)
+        """
         s_dict = {}
         v_dict = {}
         e_dict = {}
@@ -152,5 +212,95 @@ class MotifPiece:
                 s_dict[endid].append(id)
             
         return s_dict, v_dict, e_dict
+    
+    def inference(self, input_smiles):
+        mol = get_mol(input_smiles)
+        mol = sanitize(mol)
+        print(f"Number of atoms: {mol.GetNumAtoms()}")
+        if mol == None:
+            print("The data can not be identified by RDKit!")
+            return 0
+        iteration = 0
+        while True:
+            if iteration == 0:
+                s_dict, v_dict, e_dict = self.initialize(mol)
+            
+            break_all = 0
+            for subgraph_id, edge_list in s_dict.items():
+                for j in range(len(edge_list)-1):
+                    for k in range(j+1, len(edge_list)):
+                        e1 = e_dict[edge_list[j]]
+                        e2 = e_dict[edge_list[k]]
+                        v1 = v_dict[e1[0]] + v_dict[e1[1]]
+                        v2 = v_dict[e2[0]] + v_dict[e2[1]]
+                        m = list(set(v1 + v2))
+                        m_mol = get_fragment_mol(mol, m)
+                        m_smiles = get_smiles(m_mol)
+                        m_smiles = sanitize_smiles(m_smiles)
+                        if m_smiles in self.motif_vocab:
+                            key = max(s_dict)
+                            v_dict[key+1] = m
+                            # print(f"edge_list: {edge_list}")
+                            # print(f"m: {m}")
+                            # print(f"s_dict: {s_dict}")
+                            # print(f"v_dict: {v_dict}")
+                            # print(f"e_dict: {e_dict}")
+                            # print(f"e1: {e1}")
+                            # print(f"e2: {e2}")
+
+                            new_edge = set(s_dict[e1[0]]+s_dict[e1[1]]+s_dict[e2[0]]+s_dict[e2[1]])
+                            new_edge.remove(edge_list[j])
+                            new_edge.remove(edge_list[k])
+                            new_edge = list(new_edge)
+                            s_dict[key+1] = new_edge
+                            node_list = list(set(list(e1)+list(e2)))
+                            # print(f"node list:{node_list}")
+                            # print(f"new edge: {new_edge}")
+                            deleted_edge_set = [edge_list[j], edge_list[k]]
+
+                            for e in new_edge:
+                                edge = e_dict[e]
+                                element1, element2 = list(edge)
+                                if element1 not in node_list and element2 in node_list:
+                                    e_dict[e] = (element1, key+1)
+                                elif element1 in node_list and element2 not in node_list:
+                                    e_dict[e] = (element2, key+1)
+                                else:
+                                    new_value = set(s_dict[key+1])
+                                    new_value.remove(e)
+                                    s_dict[key+1] = list(new_value)
+                                    deleted_edge_set.append(e)
+
+                            deleted_subgraph_set = list(set([e1[0], e1[1], e2[0], e2[1]]))
+
+
+                            for key in deleted_subgraph_set:
+                                del s_dict[key], v_dict[key]
+
+                            for key in deleted_edge_set:
+                                del e_dict[key]
+
+                            break_all = 1
+                            break
+                    if break_all == 1:
+                        break
+                if break_all == 1:
+                    break
+            if break_all == 0:
+                break
+            iteration += 1
+        extracted_motif = {}
+        for subgraph_id, atom_list in v_dict.items():
+            m_mol = get_fragment_mol(mol, atom_list)
+            m_smiles = get_smiles(m_mol)
+            m_smiles = sanitize_smiles(m_smiles)
+            if m_smiles not in extracted_motif:
+                extracted_motif[m_smiles] = 1
+            else:
+                extracted_motif[m_smiles] += 1
+        return extracted_motif
+                                
+
+
 
 
